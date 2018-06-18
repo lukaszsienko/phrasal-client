019d836f7135ab4a410be7f074fdf0bd35abd7dc
lukaszsienko <l.sienko@stud.elka.pw.edu.pl>
2018-06-18 14:42:35 +0200

    upgrade to newest version of Phrasal wrapper

--- src/mytranslationsystem/Example.java
+++ src/mytranslationsystem/Example.java
@@ -1,51 +1,100 @@
 package mytranslationsystem;
 
 import pl.edu.pw.elka.phrasalwrapper.*;
+import pl.edu.pw.elka.phrasalwrapper.model_persistence.ModelsPersistence;
+import pl.edu.pw.elka.phrasalwrapper.translation_model.BerkeleyTranslationModel;
+import pl.edu.pw.elka.phrasalwrapper.translation_model.GizaTranslationModel;
+import pl.edu.pw.elka.phrasalwrapper.translation_model.TranslationModel;
+import pl.edu.pw.elka.phrasalwrapper.word_alignment.BerkeleyWordAlignmentModel;
+import pl.edu.pw.elka.phrasalwrapper.word_alignment.GizaWordAlignmentModel;
 
 public class Example {
 
-    public static void exampleUseCase(String englishFilePath, String foreignFilePath, String englishOnlyCorpusFilePath, String englishPartOfParallelTuningCorpusPath, String foreignPartOfParallelTuningCorpusPath) throws Exception {
-        //Firstly, lets specify parallel corpus files, corresponding lines in files consists translated sentences.
-        ParallerCorpus corpus = new ParallerCorpus(englishFilePath, foreignFilePath, englishOnlyCorpusFilePath);
-        //Tokenization and lowercasing of both files is strongly suggested. This will overwrite existing files.
-        corpus.tokenize();
-
-        //Phrasal requires alignment of parallel corpus on the level of words (word-alignment).
-        WordAlignmentModel alignmentModel = new WordAlignmentModel(corpus);
-        //This will create a subfolder "/models/aligner_output" with aligment contents. Once this was run,
-        //we can just create WordAlignmentModel object and results subfolder will be found automatically.
-        //Subfolder "/models" is created in the location of english-side corpus file.
-        alignmentModel.runWordAlignmentProcess();
-
-        //Building ngram model of specified parallel corpus. Here we have 5-gram model.
-        LanguageModel languageModel = new LanguageModel(5, corpus);
-        //This will store results in "/models/language_model" subfolder. Results are: text file with probabilites
-        //of 1, 2, 3, ... , n - grams in ARPA format and binary file representation of this model.
-        //Can be run once as well as above commands.
-        languageModel.buildLanguageModel();
-
-        //Calculation of sth called by phrasal-authors "phrase-table" which is a model of translation.
-        TranslationModel translationModel = new TranslationModel(alignmentModel, corpus);
-        //Results goes to "/models/translation_model"
-        translationModel.buildTranslationModel();
-
-        //Model tuning. Output goes to a directory in which JVM was started. You can check this using: System.getProperty("user.dir")
-        TranslationTuner tuner = new TranslationTuner(englishPartOfParallelTuningCorpusPath, foreignPartOfParallelTuningCorpusPath, corpus, languageModel, translationModel);
-        tuner.tokenizeTuningCorpus();
-        tuner.runTuning();
-
-        //Decoder represents the object providing methods to translate sentences.
-        //Decoding is a fancy name of translation process.
-        Decoder decoder = new Decoder(languageModel, translationModel, tuner);
-        decoder.runDecodingFromConsoleInInteractiveMode();
-    }
+    private ModelsPersistence modelsPersistence;
 
     public static void main(String[] args) throws Exception {
-        String englishFilePath = args[0];
-        String foreignFilePath = args[1];
-        String onlyEnglishCorpusFilePath = args[2];
-        String englishPartOfParallelTuningCorpusPath = args[3];
-        String foreignPartOfParallelTuningCorpusPath = args[4];
-        exampleUseCase(englishFilePath, foreignFilePath, onlyEnglishCorpusFilePath, englishPartOfParallelTuningCorpusPath, foreignPartOfParallelTuningCorpusPath);
+        String foreignFilePath = args[0];
+        String englishFilePath = args[1];
+        String englishOnlyCorpusFilePath = args[2];
+        String modelOutputDirPath = args[3];
+        String modelName = args[4];
+        boolean useGizaWordAligner = false;
+
+        Example example = new Example();
+        if (useGizaWordAligner) {
+            example.trainTranslationModelUsingGizaAligner(foreignFilePath, englishFilePath, englishOnlyCorpusFilePath, modelOutputDirPath, modelName);
+        } else { // use Berkeley Word Aligner
+            example.trainTranslationModelUsingBerkeleyAligner(foreignFilePath, englishFilePath, englishOnlyCorpusFilePath, modelOutputDirPath, modelName);
+        }
+
+        Decoder decoder = example.loadTranslationModel();
+
+        decoder.translateSentence("Ann has a cat");
+        decoder.translateSentence("This is my house");
+    }
+
+    public void trainTranslationModelUsingGizaAligner(String foreignFilePath, String englishFilePath, String englishOnlyCorpusFilePath, String modelOutputDirPath, String modelName) throws Exception {
+        ModelsPersistence modelsPersistence =  ModelsPersistence.createEmptyModelsDirectory(modelOutputDirPath, modelName);
+
+        final int EVERY_N_TH_GOES_TO_TUNING_SET = 14;
+        CorpusPreparer corpusPreparer = new CorpusPreparer(foreignFilePath, englishFilePath);
+        corpusPreparer.splitCorpusIntoTrainAndTuneParts(EVERY_N_TH_GOES_TO_TUNING_SET);
+        ParallelCorpus trainingCorpus = corpusPreparer.getTrainingCorpus();
+        ParallelCorpus tuningCorpus = corpusPreparer.getTuningCorpus();
+
+        TextCorpus englishMonolingualCorpus = new TextCorpus(englishOnlyCorpusFilePath);
+        englishMonolingualCorpus.tokenize();
+
+        //Building ngram model of specified parallel corpus. Here we have 5-gram model.
+        LanguageModel languageModel = new LanguageModel(5, trainingCorpus, englishMonolingualCorpus, modelsPersistence);
+        languageModel.buildLanguageModel();
+
+        //GIZA++ word alignment model
+        GizaWordAlignmentModel gizaAlignmentModel = new GizaWordAlignmentModel(trainingCorpus, modelsPersistence);
+        gizaAlignmentModel.runWordAlignmentProcess();
+        //Building translation model using GIZA++ output
+        TranslationModel translationModelGiza = new GizaTranslationModel(gizaAlignmentModel, trainingCorpus, modelsPersistence);
+        translationModelGiza.buildTranslationModel();
+
+        TranslationTuner tuner = new TranslationTuner(tuningCorpus, modelsPersistence);
+        tuner.runTuning();
+
+        this.modelsPersistence = modelsPersistence;
+    }
+
+    public void trainTranslationModelUsingBerkeleyAligner(String foreignFilePath, String englishFilePath, String englishOnlyCorpusFilePath, String modelOutputDirPath, String modelName) throws Exception {
+        ModelsPersistence modelsPersistence =  ModelsPersistence.createEmptyModelsDirectory(modelOutputDirPath, modelName);
+
+        final int EVERY_N_TH_GOES_TO_TUNING_SET = 14;
+        CorpusPreparer corpusPreparer = new CorpusPreparer(foreignFilePath, englishFilePath);
+        corpusPreparer.splitCorpusIntoTrainAndTuneParts(EVERY_N_TH_GOES_TO_TUNING_SET);
+        ParallelCorpus trainingCorpus = corpusPreparer.getTrainingCorpus();
+        ParallelCorpus tuningCorpus = corpusPreparer.getTuningCorpus();
+
+        TextCorpus englishMonolingualCorpus = new TextCorpus(englishOnlyCorpusFilePath);
+        englishMonolingualCorpus.tokenize();
+
+        //Building ngram model of specified parallel corpus. Here we have 5-gram model.
+        LanguageModel languageModel = new LanguageModel(5, trainingCorpus, englishMonolingualCorpus, modelsPersistence);
+        languageModel.buildLanguageModel();
+
+        //Phrasal requires alignment of parallel corpus on the level of words (word-alignment).
+        //Berkeley word alignment model
+        BerkeleyWordAlignmentModel berkeleyAlignmentModel = new BerkeleyWordAlignmentModel(trainingCorpus, modelsPersistence);
+        berkeleyAlignmentModel.runWordAlignmentProcess();
+        //Building translation model using Berkeley Word Aligner output
+        TranslationModel translationModelBerkeley = new BerkeleyTranslationModel(berkeleyAlignmentModel, modelsPersistence);
+        translationModelBerkeley.buildTranslationModel();
+
+        TranslationTuner tuner = new TranslationTuner(tuningCorpus, modelsPersistence);
+        tuner.runTuning();
+
+        this.modelsPersistence = modelsPersistence;
+    }
+
+    public Decoder loadTranslationModel() throws Exception {
+        Decoder decoder = new Decoder(modelsPersistence);
+        decoder.loadDecodingModel();
+        return decoder;
     }
 }
